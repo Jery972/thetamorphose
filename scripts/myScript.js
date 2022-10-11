45467f27f74f99dd0aeaa85ce0121410a5dbce33
Jery <jeremie.giudicelli@gmail.com>
2022-10-11 17:49:02 +0200

    fin menu responsive

--- scripts/myScript.js
+++ scripts/myScript.js
@@ -1,9 +1,16 @@
+let menuIsOpen = false;
+
 function swapMenuImage() {
-  let img = document.getElementById("myImg").src;
-  console.log(img);
-  if (img == "/icons/menu.png") {
-    console.log("icii");
-    document.getElementById("myImg").src = "/icons/menuClose.png";
+  burgerImg = document.getElementById("burger-img");
+  menuResponsive = document.getElementById("drop-menu-responsive");
+  if (!menuIsOpen) {
+    burgerImg.src = "icons/menuClose.png";
+    menuResponsive.style.display = "block";
+    menuIsOpen = true;
+  } else {
+    burgerImg.src = "icons/menu.png";
+    menuResponsive.style.display = "none";
+    menuIsOpen = false;
   }
   return false;
 }
