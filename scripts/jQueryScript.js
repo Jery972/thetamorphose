bfe2fe7ad56edd0821d4750793e5dce8a8eb1819
Jery <jeremie.giudicelli@gmail.com>
2022-10-21 14:32:52 -0400

    include button contact me

--- scripts/jQueryScript.js
+++ scripts/jQueryScript.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
   $(".main-head").load("includes/menu.html");
   $(".main-foot").load("includes/footer.html");
+  $(".contact-me").load("includes/contactButton.html");
 });
