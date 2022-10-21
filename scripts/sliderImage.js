cda5b177b1c6f3919dc6e25e1c14cc0e3f3d48ed
Jery <jeremie.giudicelli@gmail.com>
2022-10-21 09:46:11 -0400

    fix slider

--- scripts/sliderImage.js
+++ scripts/sliderImage.js
@@ -3,6 +3,7 @@ const itemCount = items.length;
 const nextItem = document.querySelector(".next");
 const previousItem = document.querySelector(".previous");
 let count = 0;
+let encour = false;
 
 let timer = setTimeout(() => {
   showNextItem();
@@ -10,45 +11,53 @@ let timer = setTimeout(() => {
 let fadeTimer;
 
 function showNextItem() {
-  items[count].classList.add("leaving");
-  items[count].classList.remove("active");
-  clearTimeout(fadeTimer);
+  if (!encour) {
+    encour = true;
+    items[count].classList.add("leaving");
+    items[count].classList.remove("active");
+    clearTimeout(fadeTimer);
 
-  fadeTimer = setTimeout(() => {
-    items[count].classList.remove("leaving");
-    if (count < itemCount - 1) {
-      count++;
-    } else {
-      count = 0;
-    }
+    fadeTimer = setTimeout(() => {
+      items[count].classList.remove("leaving");
+      if (count < itemCount - 1) {
+        count++;
+      } else {
+        count = 0;
+      }
 
-    items[count].classList.add("active");
-    clearTimeout(timer);
-    timer = setTimeout(() => {
-      showNextItem();
-    }, 8000);
-  }, 1400);
+      items[count].classList.add("active");
+      encour = false;
+      clearTimeout(timer);
+      timer = setTimeout(() => {
+        showNextItem();
+      }, 8000);
+    }, 1400);
+  }
 }
 
 function showPreviousItem() {
-  items[count].classList.add("leaving");
-  items[count].classList.remove("active");
-  clearTimeout(fadeTimer);
+  if (!encour) {
+    encour = true;
+    items[count].classList.add("leaving");
+    items[count].classList.remove("active");
+    clearTimeout(fadeTimer);
 
-  fadeTimer = setTimeout(() => {
-    items[count].classList.remove("leaving");
-    if (count > 0) {
-      count--;
-    } else {
-      count = itemCount - 1;
-    }
+    fadeTimer = setTimeout(() => {
+      items[count].classList.remove("leaving");
+      if (count > 0) {
+        count--;
+      } else {
+        count = itemCount - 1;
+      }
 
-    items[count].classList.add("active");
-    clearTimeout(timer);
-    timer = setTimeout(() => {
-      showPreviousItem();
-    }, 8000);
-  }, 1400);
+      items[count].classList.add("active");
+      encour = false;
+      clearTimeout(timer);
+      timer = setTimeout(() => {
+        showPreviousItem();
+      }, 8000);
+    }, 1400);
+  }
 }
 
 function keyPress(e) {
