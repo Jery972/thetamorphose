c020470f9cb4277c9e6f35cbed2c4e19f1bb7b95
Jery <jeremie.giudicelli@gmail.com>
2022-10-23 23:20:43 -0400

    video page fin

--- scripts/myScript.js
+++ scripts/myScript.js
@@ -80,3 +80,19 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
+//VIDEOS ACCORDION
+var acc = document.getElementsByClassName("acordion-video");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active-video");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
